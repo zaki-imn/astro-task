f4f1b0b5a815a772a5138966e818b6ddb336ec3c
zakiyya <zak@mail.com>
2024-09-15 12:35:15 +0400

    Feat: Update header, add netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,14 @@ import mdx from '@astrojs/mdx';
 
 import sitemap from '@astrojs/sitemap';
 
+import netlify from '@astrojs/netlify';
+
+import icon from 'astro-icon';
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: 'https://example.com',
+  integrations: [mdx(), sitemap(), icon()],
+  output: 'server',
+  adapter: netlify(),
 });
